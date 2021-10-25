1f6c43d5e6519761b194cf3a1f11869d792110df
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-25 09:22:55 +0530

    Adding data validation using express-validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,4 +21,6 @@ const UserSchema = new Schema({
     },
 })
 
-module.exports = mongoose.model('user', UserSchema)
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User;
