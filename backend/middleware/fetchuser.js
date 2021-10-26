a3362b69f9e9a0c7d49a5c05fc98ac571071a6a3
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-26 17:07:49 +0530

    Creating a middleware to decode user from JWT

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'Iamaprogrammer$21';
+
+const fetchuser = (req, res, next) => {
+    // Get the user from the jwt token and add id to req object
+    const token = req.header('auth-token')
+    if (!token) {
+        res.status(401).send({ error: 'Please authenticate using a valid token' });
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next()
+    } catch (error) {
+        res.send(401).send({ error: 'Pleae authenticate using a valid token' })
+    }
+}
+
+module.exports = fetchuser;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const User = require('../models/User')
+const User = require('../models/User');
 const router = express.Router();
-const { body, validationResult } = require('express-validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = 'Iamaprogrammer$21';
 
-// Create a User using: POST "/api/auth/". No login required
+// ROUTE 1 : Create a User using: POST "/api/auth/". No login required
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -50,18 +51,18 @@ router.post('/createuser', [
 })
 
 
-// Authenticate a User using: POST "/api/auth/". 
-router.post('/login', [ 
-    body('email', 'Enter a valid email').isEmail(), 
-    body('password', 'Password cannot be blank').exists(), 
-  ], async (req, res) => {
+// ROUTE 2 : Authenticate a User using: POST "/api/auth/login". 
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
     // If there are errors, return Bad request and the errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     try {
         let user = await User.findOne({ email })
         if (!user) {
@@ -84,4 +85,15 @@ router.post('/login', [
     }
 })
 
+// Route 3 : Get loggedin User Details using : POST "api/auth/getuser". Login required
+router.post('/getUser', fetchuser, async (req, res) => {
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password")
+        res.send(user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Internal Sever Error');
+    }
+})
 module.exports = router
