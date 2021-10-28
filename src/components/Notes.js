f1af328684047a8a02a596abc7e0f78f18a416f0
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-28 16:45:05 +0530

    Adding addNote component to iNotebook

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react'
 import noteContext from "../context/notes/noteContext";
 import NoteItem from './NoteItem';
+import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const {notes, setNotes} = context;
+    const { notes, addNote } = context;
     return (
-        <div className="row my-3">
-            <h2>Your Notes</h2>
-            {notes.map(note => {
-                return <NoteItem key={note._id} note={note}/>
-            })}
-        </div>
+        <>
+            <AddNote/>
+            <div className="row my-3">
+                <h2>Your Notes</h2>
+                {notes.map(note => {
+                    return <NoteItem key={note._id} note={note} />
+                })}
+            </div>
+        </>
     )
 }
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,38 +2,64 @@ import NoteContext from "./noteContext";
 import { useState } from 'react'
 
 const NoteState = (props) => {
-    const notesInitial = [
-  {
-    "_id": "617a1a0d8782b974396d6044",
-    "user": "6178c09c289ef0787b758104",
-    "title": "My Title1",
-    "description": "My Description",
-    "tag": "personal",
-    "__v": 0
-  },
-  {
-    "_id": "617a1a0e8782b974396d6046",
-    "user": "6178c09c289ef0787b758104",
-    "title": "My Title1",
-    "description": "My Description",
-    "tag": "personal",
-    "__v": 0
-  },
-  {
-    "_id": "617a1a0e8782b974396d6048",
-    "user": "6178c09c289ef0787b758104",
-    "title": "My Title1",
-    "description": "My Description",
-    "tag": "personal",
-    "__v": 0
+  const notesInitial = [
+    {
+      "_id": "617a1a0d8782b974396d6044",
+      "user": "6178c09c289ef0787b758104",
+      "title": "My Title1",
+      "description": "My Description",
+      "tag": "personal",
+      "__v": 0
+    },
+    {
+      "_id": "617a1a0e8782b974396d6046",
+      "user": "6178c09c289ef0787b758104",
+      "title": "My Title1",
+      "description": "My Description",
+      "tag": "personal",
+      "__v": 0
+    },
+    {
+      "_id": "617a1a0e8782b974396d6048",
+      "user": "6178c09c289ef0787b758104",
+      "title": "My Title1",
+      "description": "My Description",
+      "tag": "personal",
+      "__v": 0
+    }
+  ]
+
+  const [notes, setNotes] = useState(notesInitial);
+
+  // Add a Note
+  const addNote = (title, description, tag) => {
+    // todo: Api call
+    console.log('Adding a new note');
+    const note = {
+      "_id": "617a1a0e8782b974396d6048",
+      "user": "6178c09c289ef0787b758104",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "__v": 0
+    }
+    setNotes(notes.concat(note))
   }
-]
-const [notes, setNotes] = useState(notesInitial);
-    return (
-        <NoteContext.Provider value={{notes}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
+
+  // Delete a Note
+  const deleteNote = () => {
+
+  }
+  // Edit a Note
+  const editNote = () => {
+
+  }
+
+  return (
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+      {props.children}
+    </NoteContext.Provider>
+  )
 }
 
 export default NoteState;
