bbeda46382979d47020f5d222e872647d3e282e3
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-30 22:29:38 +0530

    adding .env files and pushing api to heroku

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const mongoURI = process.env.DATABASE_URL
 
 const connectTOMongo = async () => {
     mongoose.connect(mongoURI, () => {
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 connectTOMongo()
 
 const app = express()
-const port = 5000
+const port = 3000
 
 app.use(cors())
 app.use(express.json())
@@ -14,7 +14,4 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-
-app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000)
