d2bb7c8069dc8a2787276ae7bd50e066fa32d616
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-30 21:09:56 +0530

    Adding signup component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,17 +14,17 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password mst be atleast 5 characters').isLength({ min: 5 })
 ], async (req, res) => {
-
+    let success = false;
     // If there are errors, return Bad request and the errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+        return res.status(400).json({ success, errors: errors.array() })
     }
     // Check whether this user email exists already
     try {
         let user = await User.findOne({ email: req.body.email })
         if (user) {
-            return res.status(400).json({ error: 'Sorry a user with this email already exists' })
+            return res.status(400).json({ success, error: 'Sorry a user with this email already exists' })
         }
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(req.body.password, salt);
@@ -42,8 +42,8 @@ router.post('/createuser', [
         }
         const authtoken = jwt.sign(data, JWT_SECRET)
 
-          
-        res.json({ authtoken })
+        success = true
+        res.json({ success, authtoken })
     } catch (error) {
         console.error(error.message)
         res.status(500).send('Internal Server Error');
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
 const Signup = () => {
+    const host = 'http://localhost:5000'
+    const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" });
+
+    let history  = useHistory();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // API Call
+        const { name, email, password, cpassword } = credentials
+        const response = await fetch(`${host}/api/auth/createuser`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+        const json = await response.json()
+        console.log(json);
+        // Save the auth token and redirect
+        if(json.success){
+        localStorage.setItem('token', json.authtoken)
+        history.push("/")
+        }
+        else {
+            alert('Invalid credentials')
+        }
+
+
+    }
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
     return (
         <div>
-            I am Signup
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" name="name" id="name" onChange={onChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" name="email" id="email" onChange={onChange} />
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" name="password" id="password" onChange={onChange} minLength={5} required/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Conform Password</label>
+                    <input type="password" className="form-control" name="cpassword" id="cpassword" onChange={onChange} minLength={5} required/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
