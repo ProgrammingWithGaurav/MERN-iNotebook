f860887a39edbfa8f806cea5cc15fd39831aaecb
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-31 11:26:00 +0530

    added user details and logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Link, useLocation, useHistory } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = () => {
-    let history = useHistory();
- 
+const Navbar = () => { 
     let location = useLocation();
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 const Setting = () => {
+    let [username, setUsername] = useState("");
+    let [email, setEmail] = useState("");
+    const host = "https://my-inotebook-api.herokuapp.com"
     const history = useHistory();
     const handleLogout = () => {
         localStorage.removeItem('token')
         history.push('/login')
     }
+
+    const getUserData = async () => {
+        const response = await fetch(`${host}/api/auth/getuser`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'applicatin/json',
+                'auth-token': localStorage.getItem('token')
+            }
+        })
+        const json = await response.json();
+        setUsername(json.name)
+        setEmail(json.email)
+    }
+    
+    useEffect(() => {
+        getUserData()
+    })
     return (
         <div>
             <h3 className="fs-1">Profile</h3>
             <div className="container my-3">
-                <p className="h6 my-2">Name: </p> <li className="list-group-item">Gaurav</li>
-                <p className="h6 my-2">Email: </p> <li className="list-group-item">gaurav2499kumar@gmail.com</li>
+                <p className="h6 my-2">Name: </p> <li className="list-group-item">{username}</li>
+                <p className="h6 my-2">Email: </p> <li className="list-group-item">{email}</li>
             </div>
             <div className="my-3">
-                <button onClick={handleLogout} type="button" class="btn btn-outline-danger">Signout</button>
+                <button onClick={handleLogout} type="button" className="btn btn-outline-danger">Signout</button>
             </div>
         </div>
     )
